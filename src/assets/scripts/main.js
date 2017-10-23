3bcbe648e8d3684aa5470620e486b005235b2741
Sabrina Nedjah <snedjah78@gmail.com>
2017-10-23 10:17:53 +0200

    final push

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,17 +1,40 @@
+// Set variables
 const $shooter = document.querySelector('.shooter')
 const $score = $shooter.querySelector('.score .value')
 const $targets = $shooter.querySelector('.targets')
 let score = 0;
 
+// Create target function
 const createTarget = () => {
-    //crée un element dans le dom
+
+    // Create element
     const $target = document.createElement('div')
-    //ajout d'une classe
+
+    // Add class and style
     $target.classList.add('target')
-    $target.style.top = `${Math.random() * 100}%`
-    $target.style.left = `${Math.random() * 100}%`
-    //ajout dans la page (de la div avec la classe target dans targets)
+    $target.style.top = `${Math.random() * 88}%`
+    $target.style.left = `${Math.random() * 88}%`
+
+    $target.addEventListener('mouseenter', () => 
+        {
+        //Update score
+         score ++
+         $score.textContent = score
+
+        // Remove target
+         $targets.removeChild($target)
+
+        // Clear TimeOut
+         window.clearTimeout(timeoutID)
+         })
+
+     //add to targets element
     $targets.appendChild($target)
+    window.setTimeout(createTarget, Math.random() * 1000)
+
+   const timeoutID = window.setTimeout(() => {
+        $targets.removeChild($target)
+    }, 800)
 }
 
 createTarget ()
